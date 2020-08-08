81293823a01a5e9f063e9be71117c9e443e54efa
rockyrimer92 <67204957+rockyrimer92@users.noreply.github.com>
2020-08-08 18:31:05 -0400

    Initial commit

---
+++ app/src/main/java/com/example/runrd/diseologin/Login.java
@@ -0,0 +1,39 @@
+package com.example.juliocesar.diseologin;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    Button btn_ir_crearCuenta,iniciar_sesion;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        btn_ir_crearCuenta=findViewById(R.id.btn_ir_crearCuenta);
+        iniciar_sesion=findViewById(R.id.iniciar_sesion);
+
+
+
+        btn_ir_crearCuenta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this,SignUp.class));
+                finish();
+            }
+        });
+
+        iniciar_sesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(Login.this, "Bienvendio Usuario:", Toast.LENGTH_SHORT).show();
+                //Aquí codigo
+            }
+        });
+    }
+}
